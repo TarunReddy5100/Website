f8ce4393dba1b119c3c26d3850d49233d935678f
Tarun Reddy <tarunreddy94786@gmail.com>
2024-05-30 18:54:05 +0530

    as

--- send_email.php
+++ send_email.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = 'info@azagava.com'; // Your email address
     $subject = 'Contact Form Submission';
@@ -20,4 +20,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     echo 'Invalid request.';
 }
+?> -->
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = 'tarunreddy94786@gmail.com'; // Your email address
+    $subject = 'Contact Form Submission';
+
+    // Collect form data
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $phoneNum = $_POST['phoneNum'];
+    $companyName = $_POST['companyName'];
+    $sub = $_POST['sub'];
+    $message = $_POST['message'];
+
+    // Construct email message
+    $body = "First Name: $fname\n\nLast Name: $lname\n\nEmail Id: $email\n\nPhone Number: $phoneNum\n\nCompany Name: $companyName\n\nSubject: $sub\n\nMessage:\n\t\t\t$message";
+
+    // Send email
+    if (mail($to, $subject, $body)) {
+        echo 'Your message has been sent successfully.';
+    } else {
+        echo 'There was a problem sending your message. Please try again later.';
+    }
+} else {
+    echo 'Invalid request.';
+}
 ?>
+
+
